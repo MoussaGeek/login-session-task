cc4272dadcada5669b827b9537f12ee18336fc0d
MoussaGeek <moussadiakite1963@gmail.com>
2023-03-06 10:54:12 +0000

    sess

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to users_path, notice: t('.destroyed')
+    redirect_to new_session_path, notice: t('.destroyed')
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
-  resources :users, only: [:new, :create, :show, :edit, :destroy, :update]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :destroy, :update]
 end
